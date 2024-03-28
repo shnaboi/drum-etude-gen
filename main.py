07f3dda53b7d492f5ee7c44acb0d375f8656d77d
Shane Karner <shane.karner@gmail.com>
2024-03-28 01:36:54 -0400

    create pattern brain class to handle pattern creation

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
 import random
-from music21 import *
+# from music21 import *
+# import lilypond
+from pattern_brain import Pattern_Brain
+
+brain = Pattern_Brain()
+brain.gen_linear()
+
 
 # create paramaters for etude creation
 
 # when program is run, return a pattern
 
-STICKINGS = ["R", "L", "K"]
-
-new_pattern = []
-
-pattern_length = 8
-num = 0
-
-def create_linear_pattern(notes):
-  for i in range(notes):
-    # check if last two notes in pattern are the same
-    if len(new_pattern) >= 2 and new_pattern[-2] == new_pattern[-1]:
-      values_to_choose = [val for val in STICKINGS if val != new_pattern[-1]]
-      choice = random.choice(values_to_choose)
-    else:
-      choice = random.choice(STICKINGS)
-
-    new_pattern.append(choice)
-
-  print(new_pattern)
-
-create_linear_pattern(pattern_length)
-
-drumset = clef.PercussionClef()
-m = stream.Measure([drumset])
-m.show()
---
+++ pattern_brain.py
@@ -0,0 +1,20 @@
+import random
+
+class Pattern_Brain:
+  def __init__(self):
+    self.stickings = ["R", "L", "K"]
+    self.length = 8
+    self.pattern = []
+
+  def gen_linear(self):
+    for i in range(self.length):
+      # check if last two notes in pattern are the same
+      if len(self.pattern) >= 2 and self.pattern[-2] == self.pattern[-1]:
+        val_to_choose = [val for val in self.stickings if val != self.pattern[-1]]
+        choice = random.choice(val_to_choose)
+      else:
+        choice = random.choice(self.stickings)
+
+      self.pattern.append(choice)
+
+    print(self.pattern)
