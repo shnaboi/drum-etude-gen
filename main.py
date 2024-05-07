83f9b68371f6ccaf95203c8ccf089ba729346938
Shane Karner <shane.karner@gmail.com>
2024-05-06 23:57:10 -0400

    Create html page to display app interface and render PDF generated from lilypond

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import random
 import lilypond
 import subprocess
+from flask import Flask, render_template
 from pattern_brain import Pattern_Brain
+
+app = Flask(__name__)
 
 brain = Pattern_Brain()
 pattern = brain.gen_linear()
 
-with open("files.ly", "w") as file:
+with open("file.ly", "w") as file:
   string = """\\version "2.25.12"
 {
   c'4 e' g' b'
@@ -14,7 +17,14 @@ with open("files.ly", "w") as file:
   """
   file.write(string)
 
-subprocess.run([lilypond.executable(), "files.ly"])
+subprocess.run([lilypond.executable(), "file.ly"])
+
+@app.route('/')
+def home():
+  return render_template("./index.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
 
 # create paramaters for etude creation
 
